7ed7a3b59ae7b7ad9206069a97eedfb4e806e2d9
BacLuc <lucius.bachmann@gmx.ch>
2020-05-01 19:27:23 +0200

    controller: use trait to simplify crud actions of BlockController

--- blocks/bacluc_event_block/controller.php
+++ blocks/bacluc_event_block/controller.php
@@ -2,12 +2,12 @@
 
 namespace Concrete\Package\BaclucEventPackage\Block\BaclucEventBlock;
 
+use BaclucC5Crud\Adapters\Concrete5\Concrete5CrudController;
 use BaclucC5Crud\Adapters\Concrete5\Concrete5CurrentUrlSupplier;
 use BaclucC5Crud\Adapters\Concrete5\Concrete5Renderer;
 use BaclucC5Crud\Adapters\Concrete5\DIContainerFactory;
 use BaclucC5Crud\Controller\ActionProcessor;
 use BaclucC5Crud\Controller\ActionRegistry;
-use BaclucC5Crud\Controller\ActionRegistryFactory;
 use BaclucC5Crud\Controller\CrudController;
 use BaclucC5Crud\Controller\CurrentUrlSupplier;
 use BaclucC5Crud\Controller\Renderer;
@@ -27,8 +27,6 @@ use BaclucEventPackage\ShowErrorActionProcessor;
 use BaclucEventPackage\ViewActionRegistryFactory;
 use Concrete\Core\Block\BlockController;
 use Concrete\Core\Package\PackageService;
-use Concrete\Core\Page\Page;
-use Concrete\Core\Routing\Redirect;
 use Concrete\Core\Support\Facade\Application;
 use Concrete\Package\BaclucC5Crud\Controller as PackageController;
 use Concrete\Package\BaclucEventPackage\Controller as EventPackageController;
@@ -38,6 +36,7 @@ use DI\NotFoundException;
 use Exception;
 use Psr\Container\ContainerInterface;
 use ReflectionException;
+use RuntimeException;
 use function DI\autowire;
 use function DI\create;
 use function DI\factory;
@@ -45,17 +44,19 @@ use function DI\value;
 
 class Controller extends BlockController
 {
-
+    use Concrete5CrudController;
 
     /**
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws ReflectionException
+     * Controller constructor.
      */
-    public function view()
+    public function __construct($obj = null)
     {
-        $this->processAction($this->createCrudController()
-            ->getActionFor(ActionRegistryFactory::SHOW_TABLE, $this->bID));
+        parent::__construct($obj);
+        try {
+            $this->initializeCrud($this, $this->createCrudController(), $this->bID);
+        } catch (DependencyException | NotFoundException $e) {
+            throw new RuntimeException($e);
+        }
     }
 
     /**
@@ -68,87 +69,6 @@ class Controller extends BlockController
         $this->view();
     }
 
-    /**
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws ReflectionException
-     */
-    public function action_add_new_row_form($blockId)
-    {
-        $this->processAction($this->createCrudController()
-            ->getActionFor(ActionRegistryFactory::ADD_NEW_ROW_FORM, $blockId));
-    }
-
-    /**
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws ReflectionException
-     */
-    public function action_edit_row_form($blockId, $editId)
-    {
-        $this->processAction($this->createCrudController()
-            ->getActionFor(ActionRegistryFactory::EDIT_ROW_FORM, $blockId),
-            $editId);
-    }
-
-    /**
-     * Attention: all action method are called twice.
-     * Because this is a form submission, we stop after the function is executed
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws ReflectionException
-     */
-    public function action_post_form($blockId, $editId = null)
-    {
-        $this->processAction($this->createCrudController()
-            ->getActionFor(ActionRegistryFactory::POST_FORM, $blockId),
-            $editId);
-        if ($this->blockViewRenderOverride == null) {
-            Redirect::page(Page::getCurrentPage())->send();
-            exit();
-        }
-    }
-
-    /**
-     * @param $ignored
-     * @param $toDeleteId
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws ReflectionException
-     */
-    public function action_delete_entry($blockId, $toDeleteId)
-    {
-        $this->processAction($this->createCrudController()
-            ->getActionFor(ActionRegistryFactory::DELETE_ENTRY, $blockId),
-            $toDeleteId);
-        if ($this->blockViewRenderOverride == null) {
-            Redirect::page(Page::getCurrentPage())->send();
-            exit();
-        }
-    }
-
-    /**
-     * @throws DependencyException
-     * @throws NotFoundException
-     */
-    public function action_cancel_form($blockId)
-    {
-        $this->processAction($this->createCrudController()
-            ->getActionFor(ActionRegistryFactory::SHOW_TABLE, $blockId));
-    }
-
-    /**
-     * @param $ignored
-     * @param $toShowId
-     * @throws DependencyException
-     * @throws NotFoundException
-     */
-    public function action_show_details($blockId, $toShowId)
-    {
-        $this->processAction($this->createCrudController()
-            ->getActionFor(ActionRegistryFactory::SHOW_ENTRY_DETAILS, $blockId),
-            $toShowId);
-    }
 
     private function processAction(ActionProcessor $actionProcessor, ...$additionalParams)
     {
